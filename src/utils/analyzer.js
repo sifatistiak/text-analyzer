9d581f1b3d2a9de39f6600249be9bda7bcf54fd6
Istiak Ahamed <sifatistiak@gmail.com>
2024-02-01 00:03:05 +0600

    Implementing functions to use as analyzer

---
+++ src/utils/analyzer.js
@@ -0,0 +1,22 @@
+function countWords(text) {
+}
+
+function countCharacters(text) {
+}
+
+function countSentences(text) {
+}
+
+function countParagraphs(text) {
+}
+
+function findLongestWordsInParagraphs(text) {
+}
+
+module.exports = {
+    countWords,
+    countCharacters,
+    countSentences,
+    countParagraphs,
+    findLongestWordsInParagraphs,
+};
