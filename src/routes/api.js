ba0b63460fa5aca4e64223a73e989d962ff3e040
Istiak Ahamed <sifatistiak@gmail.com>
2024-02-01 00:59:08 +0600

    api endpoint for each function

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const analyzer = require('../utils/analyzer');
+const db = require('../db/db');
 
 const UPLOADS_DIR = './uploads/';
 
@@ -26,8 +27,39 @@ router.post('/upload', upload.single('text_analyzer_file'), (req, res) => {
 
     const filePath = req.file.path;
     const text = analyzer.readTextFromFile(filePath);
+    db.writeData({ text });
 
     res.json({ success: true });
+});
+
+router.get('/words', (req, res) => {
+    const { text } = db.readData();
+    const words = analyzer.countWords(text || '');
+    res.json({ words });
+});
+
+router.get('/characters', (req, res) => {
+    const { text } = db.readData();
+    const characters = analyzer.countCharacters(text || '');
+    res.json({ characters });
+});
+
+router.get('/sentences', (req, res) => {
+    const { text } = db.readData();
+    const sentences = analyzer.countSentences(text || '');
+    res.json({ sentences });
+});
+
+router.get('/paragraphs', (req, res) => {
+    const { text } = db.readData();
+    const paragraphs = analyzer.countParagraphs(text || '');
+    res.json({ paragraphs });
+});
+
+router.get('/longestWords', (req, res) => {
+    const { text } = db.readData();
+    const longestWords = analyzer.findLongestWordsInParagraphs(text || '');
+    res.json({ longestWords });
 });
 
 module.exports = router;
