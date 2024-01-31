64a9f136a150b962bcbd85f29984f3d274ceae77
Istiak Ahamed <sifatistiak@gmail.com>
2024-01-31 23:59:33 +0600

    using api routes in app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const api = require('./routes/api');
 
 const app = express();
 const PORT = 3000;
+
+app.use('/api', api);
+
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
